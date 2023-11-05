41543eeeeaa8429a174eee167c1ec6e94a95cdcd
TCOP <tylercain390@gmail.com>
2023-11-05 11:01:00 -0500

    Added install script

--- scripts/arm_toolchain.sh
+++ scripts/arm_toolchain.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+#Testing
 # Created by Lubos Kuzma
 # Group B - 1
 # ISS Program, SADT, SAIT
---
+++ scripts/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEPENDENCIES=("gdb-multiarch" "nasm" "qemu-user")
+
+echo "Checking annd installing dependencies..."
+for dep in "${DEPENDENCIES[@]}";do
+    if ! command -v $dep &> /dev/null; then
+        sudo apt-get install -y $dep
+    fi
+done
+
+# Copy scripts to /usr/local/bin
+echo "Installing toolchain scripts..."
+sudo cp arm_toolchain.sh /usr/local/bin/arm_toolchain
+sudo cp x86_toolchain.sh /usr/local/bin/x86_toolchain
+
+# Set executable permissions
+sudo chmod +x /usr/local/bin/arm_toolchain
+sudo chmod +x /usr/local/bin/x86_toolchain
+
+echo "Installation complete."
